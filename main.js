71cbb10a5bf361150d275121da03835c7238d4db
SyXexe <skytex19422581@gmail.com>
2019-07-10 12:43:46 +0200

    changement du bot

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 const { Client } = require("discord.js");
+const { Token, PREFIX } = require("./config");
 const client = new Client({ disableEveryone: true });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Je suis prèt !");
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") msg.reply("Pong!");
-  if (msg.content === "everyone")
-    msg.reply("@everyone, salut à tous !", { disableEveryone: false });
-  if (msg.content === "noteveryone")
-    msg.reply("@everyone (noteveryone), salut à tous !");
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.reply("Pong!");
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.reply("Ping!");
+});
 });
 
-client.login("NTk4MTUzOTQ5MzkyMTQyMzM2.XSTl4Q.rx8TsTGdW3grmz1rUqN-1Lx7fUk");
+client.login(TOKEN);
